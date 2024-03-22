33d3fa1f52d77feba6aa4b1b1e1e62002dc83c17
Olexandr-Savunin <163446395+Olexandr-Savunin@users.noreply.github.com>
2024-03-22 15:02:18 +0200

    Update api_test_dog.php

--- lesson_2/src/API/api_dog/api_test_dog.php
+++ lesson_2/src/API/api_dog/api_test_dog.php
@@ -1,7 +1,17 @@
-<?php
-    $rawApiResponse = file_get_contents(filename:"https://dog.ceo/api/breeds/image/random");
-    $apiResponse = json_decode($rawApiResponse, associative:true);
-    var_export($rawApiResponse);
-    echo "\nI am here\n";
-    echo $apiResponse["message"];
-?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Leson_2</title>
+        <meta charset="utf-8">
+    </head>
+    <body>
+         <?php
+            $rawApiResponse = file_get_contents(filename:"https://dog.ceo/api/breeds/image/random");
+            $apiResponse = json_decode($rawApiResponse, associative:true);
+            // var_export($rawApiResponse);
+            // echo "\nI am here\n";
+            // echo $apiResponse["message"];
+         ?>
+        <img src="<?= $apiResponse["message"] ?>" alt="">
+    </body>
+</html>
